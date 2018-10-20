07fcee8cee2c91c78009393154d85d924bafcece
Kun Huang <kkun.huang@gmail.com>
2018-10-20 16:54:40 -0400

    pinned tray to bottom

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -12,17 +12,39 @@ class CanvasViewController: UIViewController {
 
     @IBOutlet weak var tray: UIView!
     
+    let trayDownOffset: CGFloat! = 160
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        trayUp = tray.center // The initial position of the tray
+        trayDown = CGPoint(x: tray.center.x ,y: tray.center.y + trayDownOffset) // The position of the tray transposed down
     }
     
     
     @IBAction func panningTray(_ sender: UIPanGestureRecognizer) {
         
-        let location = sender.location(in: self.view)
-        tray.center = location
+        //let location = sender.location(in: self.view)
+        //tray.center = location
+        
+        let translation = sender.translation(in: self.view)
+        var originalCenter = tray.center
+        
+        if sender.state == .began {
+            originalCenter = tray.center
+        } else if sender.state == .changed {
+            tray.center = CGPoint(x: originalCenter.x, y: originalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            var velocity = sender.velocity(in: self.view)
+            
+            if velocity.y > 0 {
+                print("move down")
+            } else {
+                print("move up")
+            }
+        }
 
     }
     
